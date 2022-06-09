b36f067ceece680479b0592f6f6828499441a248
sanu-a <96769411+sanu-a@users.noreply.github.com>
2022-06-09 14:09:39 +0530

    cm

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,13 +21,22 @@ const db = mysql.createPool({
 app.post("/login", (req, res) => {
   const userName = req.body.userName;
   const password = req.body.password;
-  const query = "select * from users where user_name=? and password=?";
-  db.query(query, [userName, password], (err, result) => {
+
+  const query = "select * from users where user_name=?";
+
+  db.query(query, userName, (err, result) => {
     if (err) res.status(500).send("Server error");
+
     if (result.length > 0) {
-      res.status(200).send("login successful");
+      bcrypt.compare(password, result[0].password, (err, resp) => {
+        if (resp) {
+          res.status(200).send("login successful");
+        } else {
+          res.send("Please check password");
+        }
+      });
     } else {
-      res.send("Please check username/password");
+      res.send("Please check username");
     }
   });
 });
@@ -33,18 +44,25 @@ app.post("/login", (req, res) => {
 app.post("/signup", (req, res) => {
   const userName = req.body.userName;
   const password = req.body.password;
-  const query1 = "select * from users where user_name=?";
-  db.query(query1, userName, (err, result) => {
-    if (err) res.status(500).send("Server error");
-    if (result.length > 0) {
-      res.send("User name already exist!!");
-    } else {
-      const query2 = "insert into users (user_name, password) values(?,?)";
-      db.query(query2, [userName, password], (err, result) => {
-        if (err) res.status(500).send("Server error");
-        res.status(200).send("Sign up successful");
-      });
-    }
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) console.log(err);
+
+    const query1 = "select * from users where user_name=?";
+
+    db.query(query1, userName, (err, result) => {
+      if (err) res.status(500).send("Server error");
+      if (result.length > 0) {
+        res.send("User name already exist!!");
+      } else {
+        const query2 = "insert into users (user_name, password) values(?,?)";
+
+        db.query(query2, [userName, hash], (err, result) => {
+          if (err) res.status(500).send("Server error");
+          res.status(200).send("Sign up successful");
+        });
+      }
+    });
   });
 });
 
